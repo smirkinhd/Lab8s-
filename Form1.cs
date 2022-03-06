f4dd9e66679ca133c53b0785f18645080e328535
smirkinhd <smirkinhd@gmail.com>
2022-03-06 10:39:37 +0300

    Добавьте файлы проекта.

---
+++ Form1.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace lab8
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            InitializeComponent();
+            textBox1.Multiline = true; // разрешаем многострочный текст
+                                       // textBox1 занимает всю свободную поверхность формы
+            textBox1.Dock = DockStyle.Fill;
+            // включаем вертикальную и горизонтальную полосы прокрутки
+            textBox1.ScrollBars = ScrollBars.Both;
+            textBox1.WordWrap = false; // запрещаем перенос строк
+            textBox1.Clear();
+            this.Text = "Простой текстовый редактор";
+            openFileDialog1.FileName = "Text2.txt";
+            openFileDialog1.Filter = "Текстовые файлы (*.txt)|*.txt|Все файлы (*.*)|*.*";
+            saveFileDialog1.Filter = "Текстовые файлы (*.txt)|*.txt|Все файлы (*.*)|*.*";
+        }
+
+        private void открытьToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() != DialogResult.OK)
+                return;
+            //if (openFileDialog1.FileName == null) return;
+            // Чтение текстового файла:
+            try
+            { // Создание экземпляра StreamReader для чтения из файла
+                var Читатель = new System.IO.StreamReader(openFileDialog1.FileName,
+                System.Text.Encoding.GetEncoding(1251));
+                // здесь заказ кодовой страницы Winl251 для русских букв
+                textBox1.Text = Читатель.ReadToEnd();
+                Читатель.Close();
+            }
+            catch (System.IO.FileNotFoundException Ситуация)
+            {
+                MessageBox.Show(Ситуация + "\nНет такого файла", "Ошибка",
+                MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            }
+            catch (System.Exception Ситуация)
+            {
+                // Отчет о других ошибках
+                MessageBox.Show(Ситуация.Message, "Ошибка", MessageBoxButtons.OK,
+                MessageBoxIcon.Exclamation);
+            }
+        }
+
+        private void сохранитьКакToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            saveFileDialog1.FileName = openFileDialog1.FileName;
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK) Запись();
+        }
+        // Вспомогательный метод для записи текста в файл
+        private void Запись()
+        {
+            try
+            { // Создание экземпляра StreamWriter для записи в файл:
+                var Писатель = new System.IO.StreamWriter(saveFileDialog1.FileName, false,
+                System.Text.Encoding.GetEncoding(1251));
+                // - здесь заказ кодовой страницы Winl251 для русских букв
+                Писатель.Write(textBox1.Text);
+                Писатель.Close();
+                textBox1.Modified = false;
+            }
+            catch (System.Exception Ситуация)
+            { // Отчет обо всех возможных ошибках
+                MessageBox.Show(Ситуация.Message, "Ошибка", MessageBoxButtons.OK,
+                MessageBoxIcon.Exclamation);
+            }
+        }
+        // Обработчик события Click пункта меню Выход
+        private void выходToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (textBox1.Modified == false) return;
+            // Если текст модифицирован, то спросить, записывать ли файл
+            DialogResult MBox = MessageBox.Show("Текст был изменен.\nСохранить изменения?",
+            "Простой редактор", MessageBoxButtons.YesNoCancel, MessageBoxIcon.Exclamation);
+            // YES — диалог; NO — выход; CANCEL — редактировать
+            if (MBox == DialogResult.No) return;
+            if (MBox == DialogResult.Cancel) e.Cancel = true;
+            if (MBox == DialogResult.Yes)
+            {
+                if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+                {
+                    Запись();
+                    return;
+                }
+                else e.Cancel = true; // Передумал выходить из программы
+            } // DialogResult.Yes
+        }
+
+    }
+}
